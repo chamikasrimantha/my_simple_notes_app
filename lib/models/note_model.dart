04ba5360b2fe8d1ab581af23a8a5848b5e2d1e9c
chamikasrimantha <chamikasrimantha2002@gmail.com>
2024-11-03 10:15:21 +0530

    datetime fields added to the model

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -2,20 +2,25 @@ class Note {
   final int? id; // Nullable for new notes
   final String title;
   final String content;
+  final DateTime createdAt;
+  final DateTime updatedAt;
 
   Note({
     this.id,
     required this.title,
     required this.content,
+    required this.createdAt,
+    required this.updatedAt,
   });
 
-  // Convert a Note into a Map. The Map is used as the
-  // data for the database.
+  // Convert a Note into a Map. The Map is used as the data for the database.
   Map<String, dynamic> toMap() {
     return {
       'id': id,
       'title': title,
       'content': content,
+      'createdAt': createdAt.toIso8601String(),
+      'updatedAt': updatedAt.toIso8601String(),
     };
   }
 
@@ -25,6 +30,8 @@ class Note {
       id: map['id'],
       title: map['title'],
       content: map['content'],
+      createdAt: DateTime.parse(map['createdAt']),
+      updatedAt: DateTime.parse(map['updatedAt']),
     );
   }
 }
